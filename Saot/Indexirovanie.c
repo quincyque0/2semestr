25f2b0b1daa8aee89803884f065411085e9a2821
quincyque0 <email@rudometow06.ru>
2025-03-31 08:11:48 +0700

    2stepIndexSaot

--- Saot/Indexirovanie.c
+++ Saot/Indexirovanie.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-#include<string.h> // для strcmp
+#include<string.h>
 
 struct phone_book{
     char name[50];
@@ -9,6 +9,14 @@ struct phone_book{
     char patronymic[50];
     int phonenumber;
 };
+
+int* Filllne(int begin,int size,int *massive){
+    for(int i = 0; i < size; i++){
+        massive[i] = begin + i;
+    }
+    return massive;
+}
+
 
 int Less(struct phone_book A, struct phone_book B){
     return strcmp(A.name, B.name) < 0;
@@ -18,24 +26,30 @@ int More(struct phone_book A, struct phone_book B){
     return strcmp(A.name, B.name) > 0;
 }
 
-void Shaker_sort(struct phone_book arr[], int sizestruct){
+void indexprint(struct phone_book arr[],int index[],int size){
+    for(int i = 0; i < size; i++) {
+        printf("%s %s %s %d\n", arr[index[i]].name, arr[index[i]].surname, 
+            arr[index[i]].patronymic, arr[index[i]].phonenumber);
+    }
+}
+void Shaker_sort(struct phone_book arr[], int sizestruct, int index[]){
     int L = 0, R = sizestruct - 1, k = sizestruct - 1;
     do{
-        struct phone_book tmp;
+        int tmp;
         for(int i = R; i > L; i--){
             if(Less(arr[i], arr[i-1])){
-                tmp = arr[i];
-                arr[i] = arr[i-1];
-                arr[i-1] = tmp;
+                tmp = index[i];
+                index[i] = index[i-1];
+                index[i-1] = tmp;
                 k = i;
             }
         }
         L = k;
         for(int i = L; i < R; i++){
             if (More(arr[i], arr[i+1])){
-                tmp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = tmp;
+                tmp = index[i];
+                index[i] = index[i+1];
+                index[i+1] = tmp;
                 k = i;
             }
         }
@@ -44,6 +58,8 @@ void Shaker_sort(struct phone_book arr[], int sizestruct){
 }
 
 int main(){
+
+    
     struct phone_book directory[] = {
         {"Swifter","Phperov","Cssovich",9136655},
         {"VanGO","Pascalev","Asemblerovich",7770099},
@@ -53,12 +69,25 @@ int main(){
     };
     
     int size = sizeof(directory)/sizeof(directory[0]);
-    Shaker_sort(directory, size);
 
+    int *index_name = malloc(size * sizeof(int));
+    int *index_surname = malloc(size * sizeof(int));
+
+    Filllne(0,size,index_name);
+    Filllne(0,size,index_surname);
+
+    Shaker_sort(directory, size,index_name);
+
+    for(int i = 0;i < size; i++){
+        printf("%d\n",index_name[i]);
+    }
     for(int i = 0; i < size; i++) {
         printf("%s %s %s %d\n", directory[i].name, directory[i].surname, 
                directory[i].patronymic, directory[i].phonenumber);
     }
+    printf("\n");    
+
+    indexprint(directory,index_name,size);
     
     return 0;
 }
